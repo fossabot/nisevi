803ec4ad6d93df0c9a45da0ea85e88aaceaeab71
nisevi <nicolas.s.vidal@gmail.com>
2016-07-14 16:51:02 +0200

    Mailer configuration.
    Receive messages from clients and send messages to them.

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < ApplicationMailer
+  def contact_email(contact)
+    @contact = contact
+    mail(to: @contact.email, subject: 'Welcome to my site.')
+  end
+
+  def client_message(contact)
+    @contact = contact
+    mail(from: @contact.email, to: ENV["USERNAME_MAILER"], subject: 'Client message.')
+  end
+end
--- spec/mailers/previews/contact_mailer_preview.rb
+++ spec/mailers/previews/contact_mailer_preview.rb
@@ -1,4 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/contact_mailer
 class ContactMailerPreview < ActionMailer::Preview
+  def welcome_email
+    ContactMailer.contact_email(Contact.last)
+  end
 
+  def client_message
+    ContactMailer.client_message(Contact.last)
+  end
 end
