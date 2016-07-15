23a352bb5ff9c367cd8f688db2243fc087179c20
nisevi <nicolas.s.vidal@gmail.com>
2016-07-15 16:30:53 +0200

    Added through model 'UserSkill' for many to many relationship between Skills and User models.

---
+++ db/migrate/20160715135955_create_user_skills.rb
@@ -0,0 +1,13 @@
+class CreateUserSkills < ActiveRecord::Migration[5.0]
+  def change
+    create_table :user_skills do |t|
+      t.belongs_to :user, index: true
+      t.belongs_to :skill, index: true
+      t.string :description
+      t.string :url_skill
+      t.deate  :date_user_skill
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/user_skills.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user_skill do
+    user nil
+    skill nil
+    description "MyString"
+    url_skill "MyString"
+  end
+end
