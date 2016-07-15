3910b79277b6e243925f0c4df899b85ea3364718
nisevi <nicolas.s.vidal@gmail.com>
2016-07-15 17:14:43 +0200

    Mailer configuration for development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,10 +26,22 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.perform_caching = false
+
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.smtp_settings = {
+		address: ENV["ADDRESS_MAILER"],
+		port: 587,
+		domain: ENV["DOMAIN_MAILER"],
+		user_name: ENV["USERNAME_MAILER"],
+		password: ENV["PASSWORD_MAILER"],
+		authentication: :login,
+		enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
