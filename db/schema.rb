b8fc85474170a7e66a55234634e169dad6dfc2b5
nisevi <nicolas.s.vidal@gmail.com>
2016-07-15 17:48:54 +0200

    Added schema with migrations applied.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160715135955) do
+ActiveRecord::Schema.define(version: 20160715154637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20160715135955) do
     t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "header"
     t.index ["article_id"], name: "index_images_on_article_id", using: :btree
   end
 
