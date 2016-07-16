15acc5c1f7ac47c43c440410bc3895f964d7849b
nisevi <nicolas.s.vidal@gmail.com>
2016-07-16 15:24:32 +0200

    Removed unnecessary scopes for images.

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,8 +2,4 @@ class Image < ApplicationRecord
   belongs_to :article
   belongs_to :portfolio
   belongs_to :service
-
-  scope :article_header, -> { where(article: true, header:true) }
-  scope :service_header, -> { where(service: true, header:true) }
-  scope :portfolio_header, -> { where(portfolio: true, header:true) }
 end
