cde5614c8ccf7e62d07c309692c949657912f77a
nisevi <nicolas.s.vidal@gmail.com>
2016-07-16 15:24:54 +0200

    Added scope to get social links.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,8 @@
 class Link < ApplicationRecord
   has_many :user_links
   has_many :users, through: :user_links
+
+  SOCIAL_LINKS = ['github', 'stack-overflow','google-plus', 'twitter', 'linkedin']
+
+  scope :social_links, -> { where("name ILIKE ANY (ARRAY[?])", SOCIAL_LINKS.map{|link| "%#{link}%"}) }
 end
