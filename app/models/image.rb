72fe523ceca07b565d0b3a12de7dd332cbdfec6d
nisevi <nicolas.s.vidal@gmail.com>
2016-07-16 11:22:15 +0200

    Added scopes to handle header images.

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,8 @@ class Image < ApplicationRecord
   belongs_to :article
   belongs_to :portfolio
   belongs_to :service
+
+  scope :article_header, -> { where(article: true, header:true) }
+  scope :service_header, -> { where(service: true, header:true) }
+  scope :portfolio_header, -> { where(portfolio: true, header:true) }
 end
