70a3dda81f2fda63f272361ed46488e71ddd9d6a
nisevi <nicolas.s.vidal@gmail.com>
2016-07-24 12:39:54 +0200

    Added new column "date_of_birth" to "user" model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160716110925) do
+ActiveRecord::Schema.define(version: 20160724101925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -182,6 +182,7 @@ ActiveRecord::Schema.define(version: 20160716110925) do
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
     t.string   "presentation"
+    t.date     "date_of_birth"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
