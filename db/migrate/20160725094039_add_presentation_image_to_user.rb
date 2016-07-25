fbe79437337c35b828b11d1f73713e070fde95aa
nisevi <nicolas.s.vidal@gmail.com>
2016-07-25 11:41:23 +0200

    Add presentation image to the user model.

---
+++ db/migrate/20160725094039_add_presentation_image_to_user.rb
@@ -0,0 +1,5 @@
+class AddPresentationImageToUser < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :presentation_image, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725082925) do
+ActiveRecord::Schema.define(version: 20160725094039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -184,6 +184,7 @@ ActiveRecord::Schema.define(version: 20160725082925) do
     t.boolean  "admin",                  default: false
     t.string   "presentation"
     t.date     "date_of_birth"
+    t.string   "presentation_image"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
