b345f32017cdf76e1707a8f99658d8507a774351
nisevi <nicolas.s.vidal@gmail.com>
2016-08-17 18:48:45 +0200

    Management console controller for the index action.

--- app/controllers/management_console_controller.rb
+++ app/controllers/management_console_controller.rb
@@ -2,5 +2,11 @@ class ManagementConsoleController < ApplicationController
   before_action :verify_user_is_admin
 
   def index
+    @profile = User.find_by_admin(:true)
+    @users = User.all.order("id DESC").page(params[:page]).per(10)
+    @services = Service.all.order("id DESC").page(params[:page]).per(10)
+    @works = Portfolio.all.order("id DESC").page(params[:page]).per(10)
+    @articles = Article.all.order("id DESC").page(params[:page]).per(10)
+    @comments = Comment.all.order("id DESC").page(params[:page]).per(10)
   end
 end
