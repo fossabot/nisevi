be85651b9f889dd2a29d1b517ec0fc4708dca8ba
nisevi <nicolas.s.vidal@gmail.com>
2016-08-18 13:46:03 +0200

    Elastic search callbacks added to model article in order to have the CRUD of the objects in the generated index.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,11 @@
+require 'elasticsearch/model'
+
 class Article < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
 
   validates :title, presence: true, length: { maximum: 100 }
   validates :content, presence: true
