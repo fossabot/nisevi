e85ffb06cb8bf0ff91d2c78c237647be87493113
nisevi <nicolas.s.vidal@gmail.com>
2016-11-17 18:10:00 +0100

    Setup timeout of the session to 15 minutes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,8 +45,7 @@ class User < ApplicationRecord
   has_many :user_links, dependent: :destroy
   has_many :links, through: :user_links
 
-  # Include devise modules. Others available are ':lockable' and ':timeoutable'
-  devise :database_authenticatable, :registerable, :omniauthable,
+  devise :database_authenticatable, :registerable, :omniauthable, :timeoutable,
          :confirmable, :recoverable, :rememberable, :trackable, :validatable
 
   validates :email, presence: true, uniqueness: true
